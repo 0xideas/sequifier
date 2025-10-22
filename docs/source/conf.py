877091a229afbdb3bb3dad72ef88974223dc40b0
leon luithlen <leontimnaluithlen@gmail.com>
2025-10-22 19:05:32 +0200

    add easteregg (#69)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'sequifier'
 copyright = '2025, Leon Luithlen'
 author = 'Leon Luithlen'
-release = 'v0.8.0.7'
+release = 'v0.8.0.8'
 html_baseurl = 'https://www.sequifier.com/'
 
 # -- General configuration ---------------------------------------------------
