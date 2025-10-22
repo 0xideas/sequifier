360d79b8bbcefc56fbbdf00de2fd3eb864260fd1
leon luithlen <leontimnaluithlen@gmail.com>
2025-10-22 16:39:39 +0200

    reintroduce pip install . (#63)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'sequifier'
 copyright = '2025, Leon Luithlen'
 author = 'Leon Luithlen'
-release = '0.8.0.1'
+release = '0.8.0.2'
 html_baseurl = 'https://www.sequifier.com/'
 
 # -- General configuration ---------------------------------------------------
