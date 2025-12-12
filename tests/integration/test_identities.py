5cce6b233a5f4901536194323f22a1c953f682d6
Leon Luithlen <leontimnaluithlen@gmail.com>
2025-12-12 18:46:01 +0100

    refactor test_identities

--- tests/integration/test_identities.py
+++ tests/integration/test_identities.py
@@ -4,27 +4,52 @@ import pytest
 
 @pytest.mark.optional
 def test_identities(targets, predictions, probabilities, embeddings):
+    failed_models = []
     for model_name, preds in predictions.items():
-        print(f"{model_name = }")
         equal = preds.to_numpy() == targets["preds"][model_name].to_numpy()
         mean_equal = np.mean(equal.astype(int))
         if model_name != "model-real-1-best-3-autoregression":
-            assert (
-                mean_equal == 1.0
-            ), f"{model_name} preds are not identical to target: {preds.to_numpy() = } != {targets['preds'][model_name].to_numpy() = }: {equal = }, {mean_equal = }"
+            if not mean_equal == 1.0:
+                failed_models.append(
+                    (
+                        model_name,
+                        equal,
+                        f"{model_name} preds are not identical to target: {preds.to_numpy() = } != {targets['preds'][model_name].to_numpy() = }: {equal = }, {mean_equal = }",
+                    )
+                )
         else:
-            assert (
-                mean_equal < 1.0
-            ), f"{model_name} preds are not randomized, {preds.to_numpy() = } == {targets['preds'][model_name].to_numpy() = }: {equal = }, {mean_equal = }"
+            if mean_equal == 1.0:
+                failed_models.append(
+                    (
+                        model_name,
+                        equal,
+                        f"{model_name} preds are not randomized, {preds.to_numpy() = } == {targets['preds'][model_name].to_numpy() = }: {equal = }, {mean_equal = }",
+                    )
+                )
 
     for model_name, probs in probabilities.items():
-        print(f"{model_name = }")
-        all_equal = np.all(probs.to_numpy() == targets["probs"][model_name].to_numpy())
-        assert all_equal, f"{model_name} probs are not identical to target"
+        equal = probs.to_numpy() == targets["probs"][model_name].to_numpy()
+        all_equal = np.all(equal)
+        if not all_equal:
+            failed_models.append(
+                (model_name, equal, f"{model_name} probs are not identical to target")
+            )
 
     for model_name, embeds in embeddings.items():
-        print(f"{model_name = }")
-        all_equal = np.all(
-            embeds.to_numpy() == targets["embeds"][model_name].to_numpy()
-        )
-        assert all_equal, f"{model_name} embeddings are not identical to target"
+        equal = embeds.to_numpy() == targets["embeds"][model_name].to_numpy()
+        all_equal = np.all(equal)
+        if not all_equal:
+            failed_models.append(
+                (
+                    model_name,
+                    equal,
+                    f"{model_name} embeddings are not identical to target",
+                )
+            )
+
+    if len(failed_models):
+        print(failed_models)
+        failed_models_subset = [(model, message) for model, _, message in failed_models]
+        assert (
+            len(failed_models) == 0
+        ), f"{len(failed_models) = } - {failed_models_subset}"
